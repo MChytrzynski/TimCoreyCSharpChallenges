28802c35e81c5967bda07987c62749fabfe22825
Chytrvs <marcinchytrzynski@gmail.com>
2019-11-07 15:55:20 +0100

    Primary challenge

---
+++ LINQ Challenge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimaryChallenge("asdfxcb");
+            System.Console.WriteLine("asdf");
+        }
+        static List<char> PrimaryChallenge(string input){
+            return input.OrderBy(x=>x).ToList();
+        }
+    }
+}
