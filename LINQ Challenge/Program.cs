1265a884289ba8406cfe158293a8e6c513b3e67a
Chytrvs <marcinchytrzynski@gmail.com>
2019-11-07 20:21:13 +0100

    Bonus challenge

--- LINQ Challenge/Program.cs
+++ LINQ Challenge/Program.cs
@@ -8,11 +8,23 @@ namespace LINQ_Challenge
     {
         static void Main(string[] args)
         {
-            PrimaryChallenge("asdfxcb");
-            System.Console.WriteLine("asdf");
+            PrimaryChallenge("aasdfxcb");
+            BonusChallenge("aassddddffxcbss");
+
         }
-        static List<char> PrimaryChallenge(string input){
-            return input.OrderBy(x=>x).ToList();
+        static List<char> PrimaryChallenge(string input)
+        {
+            return input.OrderBy(x => x).ToList();
+        }
+        static Object BonusChallenge(string input)
+        {
+            input.Count();
+            return input.Select(x => new
+            {
+                Letter = x,
+                Count = input.Count(y => y == x)
+            }).OrderByDescending(x => x.Count).ThenBy(x => x.Letter).ToList();
+
         }
     }
 }
