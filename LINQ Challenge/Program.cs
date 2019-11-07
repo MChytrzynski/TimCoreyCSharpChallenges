c23c3cc6c7e15092fd2db6105f32d2c1efa0a1fa
Chytrvs <marcinchytrzynski@gmail.com>
2019-11-07 20:27:23 +0100

    Added challenge description

--- LINQ Challenge/Program.cs
+++ LINQ Challenge/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿// Primary Challenge
+// Create a LINQ query that takes in a string and returns a
+// list of all the letters in order, regardless of case. 
+ 
+// Bonus Challenge
+// Modify the LINQ query to return a list of anonymous objects,
+// each with a Letter property and a Count property.
+// Populate the count with the number of times a letter is used.
+// Order the list by letter count (max first) and then by character.
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
