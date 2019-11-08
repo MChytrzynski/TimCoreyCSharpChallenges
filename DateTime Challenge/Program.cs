1e08b47804f1700776da9e6225dc187eee1304a4
Chytrvs <marcinchytrzynski@gmail.com>
2019-11-08 22:49:35 +0100

    DateTime primary challenge

---
+++ DateTime Challenge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Datetime_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputDate ="02.11.19 12:45";
+            PrimaryChallenge(inputDate);
+        }
+        static void PrimaryChallenge(string inputDate){
+            DateTime date=new DateTime();
+            DateTime.TryParse(inputDate, out date);
+            TimeSpan DateDifference = DateTime.UtcNow.Subtract(date);
+            Console.WriteLine($"{DateDifference.Days} days and {DateDifference.Hours} hours passed since {inputDate}");
+        }
+
+    }
+}
